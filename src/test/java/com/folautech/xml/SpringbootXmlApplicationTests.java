7e9665bf2a771830246d9f3dcb03a543f1b5a71c
folaulau <folaukaveinga@gmail.com>
2022-12-15 15:24:58 -0700

    setup

---
+++ src/test/java/com/folautech/xml/SpringbootXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folautech.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
